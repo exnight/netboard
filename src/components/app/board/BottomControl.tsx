161b6903705c385f01155e45fdcb76db0681e767
exnight <exnight@users.noreply.github.com>
2020-06-13 14:58:22 +0800

    add draw tool selection elements

--- src/components/app/board/BottomControl.tsx
+++ src/components/app/board/BottomControl.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useContext, useState } from 'react';
 
 import Player from './Player';
 import Ball from './Ball';
@@ -10,12 +11,16 @@ interface Props {
   players: [string[], string[]];
 }
 
+const toolClassName =
+  'w-full px-4 py-2 hover:bg-indigo-500 hover:text-white focus:outline-none';
+
 const BottomControl: React.FC<Props> = (props) => {
   const { players } = props;
   const { setCoorState } = useContext(CoorContext);
+  const [toolIsOpen, setToolIsOpen] = useState(false);
 
   return (
-    <div className="w-full flex mt-4">
+    <div className="w-full flex my-4">
       <div className="w-1/2 flex flex-col px-8">
         <div className="flex justify-center">
           {players[0].map((pos: string) => {
@@ -46,8 +51,63 @@ const BottomControl: React.FC<Props> = (props) => {
       </div>
       <div className="w-1/2 flex px-8 justify-around items-center">
         <Ball />
-        <button type="button" onClick={() => resetAllCoor(setCoorState)}>
-          Clear All Positions
+        <div className="relative">
+          {toolIsOpen ? (
+            <>
+              <button
+                type="button"
+                aria-label="select tool"
+                tabIndex={-1}
+                className="fixed w-full h-full inset-0 bg-black opacity-75"
+                onClick={() => setToolIsOpen(false)}
+                onKeyDown={() => setToolIsOpen(false)}
+              />
+              <div className="absolute bottom-0 py-1 mb-12 bg-white rounded-lg shadow-xl">
+                <button
+                  type="button"
+                  className={toolClassName}
+                  onClick={() => setToolIsOpen(false)}
+                >
+                  Dashed Arrow
+                </button>
+                <button
+                  type="button"
+                  className={toolClassName}
+                  onClick={() => setToolIsOpen(false)}
+                >
+                  Solid Arrow
+                </button>
+                <button
+                  type="button"
+                  className={toolClassName}
+                  onClick={() => setToolIsOpen(false)}
+                >
+                  Move
+                </button>
+              </div>
+            </>
+          ) : null}
+
+          <button
+            type="button"
+            onClick={() => setToolIsOpen(!toolIsOpen)}
+            className="relative z-10 bg-white text-gray-600 hover:bg-gray-500 hover:text-white focus:outline-none focus:shadow-outline py-2 px-4 border border-gray-400 rounded shadow"
+          >
+            Select Tool
+          </button>
+        </div>
+        <button
+          type="button"
+          className="bg-white text-red-600 hover:bg-red-500 hover:text-white focus:outline-none focus:shadow-outline font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        >
+          Clear Drawings
+        </button>
+        <button
+          type="button"
+          onClick={() => resetAllCoor(setCoorState)}
+          className="bg-white text-red-600 hover:bg-red-500 hover:text-white focus:outline-none focus:shadow-outline font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        >
+          Reset Positions
         </button>
       </div>
     </div>
